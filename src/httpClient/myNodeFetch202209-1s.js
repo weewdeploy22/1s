4dfcab6a7b5d33b03bc637a92d28a311e5f31b6b
weejobs <weejobs@icloud.com>
2022-10-03 10:07:09 +0700

    update "numConn = 250"

--- src/httpClient/myNodeFetch202209-1s.js
+++ src/httpClient/myNodeFetch202209-1s.js
@@ -817,7 +817,8 @@ export async function main(
     // conn
     // const numConn = 2;
     // const numConn = 20;
-    const numConn = 50;
+    // const numConn = 50;
+    const numConn = 250;
     for (let i = 0; i < numConn; i++) {
         doingTasks.push(
             poolConn({ conn_i: i, options, url, key, })
